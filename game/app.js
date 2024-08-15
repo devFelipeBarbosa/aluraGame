2654e48ae5bbd1e6a7576e6f4dc0fa4023dd004f
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2024-08-15 18:22:35 -0300

    a

--- game/app.js
+++ game/app.js
@@ -7,22 +7,56 @@
 // let aba = document.querySelector('title');
 // aba.innerHTML = 'JS Game - Secreto';
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
 
+}
+
+exibirMensagemInicial() 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    if(chute==numeroSecreto){
+    if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`;
+        exibirTextoNaTela('p', mensagemTentativas);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor.');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior.');
+        }
+        tentativas++;
+        limparCampo()
     }
 }
 
-function gerarNumeroAleatorio(){
+function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
